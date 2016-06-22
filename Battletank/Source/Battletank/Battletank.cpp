0891aeacde575d373b47108b8fb016c070e86d2e
Jonas Kristoffersen <nazzergames@live.no>
2016-06-22 19:41:22 +0200

    Lecture 97

---
+++ Battletank/Source/Battletank/Battletank.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Battletank.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Battletank, "Battletank" );
